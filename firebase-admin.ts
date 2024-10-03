29d860d67ebd7c610f53ad97e86b117e46570119
PSeshpavan <pseshpavan@gmail.com>
2024-10-03 17:24:16 +0530

    Deploying

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -1,14 +1,17 @@
 import { FirestoreAdapter, initFirestore } from "@auth/firebase-adapter";
 import admin from "firebase-admin";
+// import serviceAccount from "./serviceAccKey.json";
 
 let app;
+
+
 
 if(!admin.apps.length){
     app = admin.initializeApp({
         credential: admin.credential.cert({
             projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY
+            privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
         }),
     });
 }
@@ -17,9 +20,20 @@ const adminDb = initFirestore({
     credential: admin.credential.cert({
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     })
 })
+
+
+// if(!admin.apps.length){
+//     app = admin.initializeApp({
+//         credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+//     });
+// }
+
+// const adminDb = initFirestore({
+//     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+// })
 
 const adminAuth = admin.auth(app);
 
