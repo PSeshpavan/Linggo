160660bce3a474d0211dac9994323d4baa49a580
PSeshpavan <pseshpavan@gmail.com>
2024-10-03 16:58:41 +0530

    Trying to Deploy

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -6,7 +6,7 @@ let app;
 if(!admin.apps.length){
     app = admin.initializeApp({
         credential: admin.credential.cert({
-            projectId: process.env.FIREBASE_PROJECT_ID,
+            projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
             privateKey: process.env.FIREBASE_PRIVATE_KEY
         }),
@@ -15,7 +15,7 @@ if(!admin.apps.length){
 
 const adminDb = initFirestore({
     credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         privateKey: process.env.FIREBASE_PRIVATE_KEY
     })
